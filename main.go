800015e4d708c28267f7532ec567a1300911b854
Nota30 <harenkeshaav11@gmail.com>
2025-06-17 11:52:12 -0400

    test

--- main.go
+++ main.go
@@ -20,29 +20,34 @@ func main() {
 		log.Fatal(err)
 	}
 
+	var mailstring string
+
 	vdlist := ctrls.Controller[0].ResponseData.VDLIST
 	vdlist[0].State = "Test"
 	for i := 0; i < len(vdlist); i++ {
 		if (vdlist[i].State != "Optl") {
-			fmt.Println("Uh oh! Looks like something is wrong with a Virtual Drive....")
-			fmt.Println("-------------------------------- Details --------------------------------")
-			fmt.Println("- DG/VD = " + vdlist[i].DGVD)
-			fmt.Println("- TYPE = " + vdlist[i].TYPE)
-			fmt.Println("- NAME = " + vdlist[i].Name)
-			fmt.Println("- STATE = " + vdlist[i].State)
-			fmt.Println("----------------------------------------------------------------")
+			mailstring = mailstring + "\nUh oh! Looks like something is wrong with a Virtual Drive...." + 
+			"\n-------------------------------- Details --------------------------------" +
+			"\n- DG/VD = " + vdlist[i].DGVD +
+			"\n- DG/VD = " + vdlist[i].DGVD +
+			"\n- TYPE = " + vdlist[i].TYPE +
+			"\n- NAME = " + vdlist[i].Name +
+			"\n- STATE = " + vdlist[i].State +
+			"\n----------------------------------------------------------------"
 		}
 	}
 
 	pdlist := ctrls.Controller[0].ResponseData.PDLIST
 	for i := 0; i < len(pdlist); i++ {
 		if (pdlist[i].State != "Onln") {
-			fmt.Println("Uh oh! Looks like something is wrong with a Physical Drive....")
-			fmt.Println("-------------------------------- Details --------------------------------")
-			fmt.Println("- EID:Slot = " + pdlist[i].EIDSlt)
-			fmt.Println("- Model = " + pdlist[i].Model)
-			fmt.Println("- STATE = " + pdlist[i].State)
-			fmt.Println("----------------------------------------------------------------")
+			mailstring = mailstring + "\nUh oh! Looks like something is wrong with a Physical Drive...." +
+			"\n-------------------------------- Details --------------------------------" +
+			"\n- EID:Slot = " + pdlist[i].EIDSlt +
+			"\n- Model = " + pdlist[i].Model +
+			"\n- STATE = " + pdlist[i].State +
+			"\n----------------------------------------------------------------"
 		}
 	}
+
+	fmt.Println(mailstring)
 }
