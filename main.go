76909bb47f3cbe4aeefd34612b6f4d56fa4faed2
Nota30 <harenkeshaav11@gmail.com>
2025-06-17 09:41:44 -0400

    path change

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 
 func main() {
-	out, err := exec.Command("date").Output()
+	out, err := exec.Command("sudo /opt/MegaRAID/storcli/storcli64 /c0 show all J").Output()
 
 	if err != nil {
 		log.Fatal(err)
